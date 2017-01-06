9e57d098664b2e9fdd5e04129d7db63d6a65a622
Christian Kaenzig <christian.kaenzig@camptocamp.com>
2017-01-06 16:53:46 +0100

    Add automatic restoration check capability

--- 9.5/docker-entrypoint.sh
+++ 9.5/docker-entrypoint.sh
@@ -27,6 +27,35 @@ gosu postgres pghoard_restore get-basebackup --config pghoard.json --site $PGHOA
 echo "Start the pghoard daemon ..."
 gosu postgres pghoard --short-log --config /home/postgres/pghoard.json &
 
-echo "Start PostgresSQL ..."
-exec gosu postgres postgres -D restore
+if [ -z "$RESTORE_CHECK_COMMAND" ]; then
+  # Manual mode
+  # Just start PostgreSQL
+  echo "Start PostgresSQL ..."
+  exec gosu postgres postgres -D restore
+
+else
+  # Automatic test mode
+  # Run test commands against PostgreSQL server and exit
+  echo "Start PostgresSQL ..."
+  gosu postgres pg_ctl -D restore start
+
+  # Give postgres some time before starting the harassment
+  sleep 20
+
+  until gosu postgres psql -At -c "SELECT * FROM pg_is_in_recovery()" | grep -q f
+  do
+    sleep 5
+    echo "AutoCheck: waiting for restoration to finish..."
+  done
+
+  echo "AutoCheck: running command on db..."
+  OUT_LINES=$(gosu postgres psql -c "$RESTORE_CHECK_COMMAND" "$RESTORE_CHECK_DB" | wc -l)
+  echo "AutoCheck: $OUT_LINES lines returned"
+
+  if [ $OUT_LINES -gt 0 ]
+  then echo "AutoCheck: SUCCESS"
+  else echo "AutoCheck: FAILURE"
+  fi
+
+fi
 
